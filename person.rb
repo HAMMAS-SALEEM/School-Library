08899bd252dd6f968e5c6a3789c2971e1de91b08
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-06 15:09:21 +0500

    Added Interface
    Inherited Person from Nameable

--- person.rb
+++ person.rb
@@ -1,9 +1,15 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..9999)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   attr_reader :id
