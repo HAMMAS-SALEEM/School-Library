e43b1d9245f558194390c03bb9d41f9c8119b1ef
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-06 16:14:20 +0500

    Update decorator
    Fix Linters

--- person.rb
+++ person.rb
@@ -1,4 +1,4 @@
-require './nameable'
+require './decorator'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -6,6 +6,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   def correct_name
