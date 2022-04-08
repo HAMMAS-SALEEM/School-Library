a5413e221b6cc0a78f61c80b8801a1438bd819bd
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-09 03:43:37 +0500

    :art: Almost Finished

--- classes/app.rb
+++ classes/app.rb
@@ -135,7 +135,13 @@ class App
   end
 
   def list_rentals
-    @rentals.each { |i| puts "Date: #{i.date} Person: #{i.person} Book: #{i.book}" }
+    puts 'Select id of any person'
+    @people.each { |i| puts "id: #{i.id}, Person: #{i.name}" }
+    print 'Person id: '
+    person_id = gets.chomp
+    @rentals.each do |i|
+      puts "Date: #{i.date}, Book: '#{i.book.title}' by #{i.book.author}" if i.person.id.to_i == person_id.to_i
+    end
   end
 
   def convert_date(str)
